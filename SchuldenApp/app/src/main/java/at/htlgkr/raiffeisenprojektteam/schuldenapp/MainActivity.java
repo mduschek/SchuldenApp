a40962a46a3e2c86b9ed10acfad646cf26d31ea9
Alexander Perndorfer <alexander-perndorfer@hotmail.com>
2016-11-09 09:32:24 +0100

    Layout Grundgerüst

--- SchuldenApp/app/src/main/java/at/htlgkr/raiffeisenprojektteam/schuldenapp/MainActivity.java
+++ SchuldenApp/app/src/main/java/at/htlgkr/raiffeisenprojektteam/schuldenapp/MainActivity.java
@@ -1,13 +1,18 @@
 package at.htlgkr.raiffeisenprojektteam.schuldenapp;
 
+import android.support.v4.view.PagerTitleStrip;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ViewPager viewPager;
+    private PagerTitleStrip titleStrip;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
