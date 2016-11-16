1a5fb56d19b76fe253a492a3d1d55866fb5f0d21
Alexander Perndorfer <alexander-perndorfer@hotmail.com>
2016-11-16 16:38:36 +0100

    Regions

---
+++ SchuldenApp/app/src/main/java/at/htlgkr/raiffeisenprojektteam/schuldenapp/Dept.java
@@ -0,0 +1,13 @@
+package at.htlgkr.raiffeisenprojektteam.schuldenapp;
+
+/**
+ * Created by Alexander on 16.11.16.
+ */
+
+public class Dept
+{
+    private String deptorName, usuage, iBan;
+    private double value;
+
+
+}
--- SchuldenApp/app/src/main/java/at/htlgkr/raiffeisenprojektteam/schuldenapp/TblWhoOwesMe.java
+++ SchuldenApp/app/src/main/java/at/htlgkr/raiffeisenprojektteam/schuldenapp/TblWhoOwesMe.java
@@ -21,7 +21,7 @@ public class TblWhoOwesMe {
     public static final String PERS_WHO_OWES_ME_FIRSTNAME="pers_who_owes_me_firstname";
     public static final String PERS_WHO_OWES_ME_LASTNAME="pers_who_owes_me_lastname";
     public static final String PERS_WHO_OWES_ME_VALUE="pers_who_owes_me_value";
-    public static final String PERS_WHO_OWES_ME_COMMENT="pers_who_owes_me_comment";
+    public static final String PERS_WHO_OWES_ME_USUAGE="pers_who_owes_me_usuage";
 
     public static final String SQL_CREATE_TABLE="CREATE TABLE "+TABLE_NAME+"("+
             PERS_WHO_OWES_ME_ID+" PRIMARY KEY NUMBER,"+
@@ -32,7 +32,7 @@ public class TblWhoOwesMe {
             PERS_WHO_OWES_ME_FIRSTNAME+" VARCHAR2," +
             PERS_WHO_OWES_ME_LASTNAME+" VARCHAR2," +
             PERS_WHO_OWES_ME_VALUE+" DOUBLE," +
-            PERS_WHO_OWES_ME_COMMENT+" VARCHAR2);";
+            PERS_WHO_OWES_ME_USUAGE+" VARCHAR2);";
 
     public static final String SQL_DROP_TABLE="DROP TABLE "+TABLE_NAME+";";
 }
