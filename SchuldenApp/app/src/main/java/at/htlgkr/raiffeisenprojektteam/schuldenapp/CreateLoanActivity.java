5682e2ced25b3cd435826c135792370a21123f93
mduschek <duschek54321@gmail.com>
2016-11-30 12:55:01 +0100

    Layout, Choose method to send, yada yada yada

--- SchuldenApp/app/src/main/java/at/htlgkr/raiffeisenprojektteam/schuldenapp/CreateLoanActivity.java
+++ SchuldenApp/app/src/main/java/at/htlgkr/raiffeisenprojektteam/schuldenapp/CreateLoanActivity.java
@@ -1,11 +1,15 @@
 package at.htlgkr.raiffeisenprojektteam.schuldenapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 /**
  * Created by Alexander on 21.11.16.
@@ -13,25 +17,57 @@ import android.widget.ListView;
 
 public class CreateLoanActivity extends AppCompatActivity
 {
-    /*EditText iban, firstname, lastname, value,usuage;
-    ListView transfer;
-    Button btnOk;
-    */
+    TextView textViewCreateLoanDescription;
+    Button buttonManualInput, buttonBluetooth, buttonNfc, buttonSms, buttonWhatsapp;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_create_loan);
 
-        /*
         Dept d = (Dept) getIntent().getSerializableExtra("object");
 
-        iban = (EditText) findViewById(R.id.editTextIban);
-        firstname = (EditText) findViewById(R.id.editTextFirstName);
-        lastname = (EditText) findViewById(R.id.editTextLastName);
-        value = (EditText) findViewById(R.id.editTextSum);
-        usuage = (EditText) findViewById(R.id.editTextUsuage);
+        textViewCreateLoanDescription = (TextView) findViewById(R.id.textViewCreateLoanDescription);
 
-        btnOk = (Button) findViewById(R.id.btnOk);
-        */
+        buttonManualInput = (Button) findViewById(R.id.buttonManualInput);
+        buttonBluetooth = (Button) findViewById(R.id.buttonBluetooth);
+        buttonNfc = (Button) findViewById(R.id.buttonNfc);
+        buttonSms = (Button) findViewById(R.id.buttonSms);
+        buttonWhatsapp= (Button) findViewById(R.id.buttonWhatsapp);
+    }
+
+    public void onButtonPressed (View source){
+        Intent intent;
+        switch (source.getId()){
+            case R.id.buttonManualInput:
+                intent = new Intent(this,DetailActivity.class);
+                intent.putExtra("object", -1);
+                startActivity(intent);
+            break;
+            case R.id.buttonBluetooth:
+                //an dieser Stelle Bluetooth aktivieren
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+                sendIntent.setType("text/plain");
+                startActivity(Intent.createChooser(sendIntent, "Titel"));
+                break;
+            case R.id.buttonNfc:
+                //an dieser Stelle NFC aktivieren
+                //intent = new Intent(this,DetailActivity.class);
+                //intent.putExtra("object", -1);
+                //startActivity(intent);
+                break;
+            case R.id.buttonSms:
+                //intent = new Intent(this,DetailActivity.class);
+                //intent.putExtra("object", -1);
+                //startActivity(intent);
+                break;
+            case R.id.buttonWhatsapp:
+                //intent = new Intent(this,DetailActivity.class);
+                //intent.putExtra("object", -1);
+                //startActivity(intent);
+                break;
+        }
     }
 }
--- SchuldenApp/app/src/main/java/at/htlgkr/raiffeisenprojektteam/schuldenapp/DetailFragment.java
+++ SchuldenApp/app/src/main/java/at/htlgkr/raiffeisenprojektteam/schuldenapp/DetailFragment.java
@@ -8,9 +8,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+@Deprecated
 public class DetailFragment extends Fragment
 {
     private View view;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
