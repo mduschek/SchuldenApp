a749fe5db0a5e52fe0fd904f2ec47aeea2f811e2
mduschek <duschek54321@gmail.com>
2016-12-01 09:42:44 +0100

    share logic v1

--- SchuldenApp/app/src/main/java/at/htlgkr/raiffeisenprojektteam/schuldenapp/CreateLoanActivity.java
+++ SchuldenApp/app/src/main/java/at/htlgkr/raiffeisenprojektteam/schuldenapp/CreateLoanActivity.java
@@ -5,10 +5,9 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.TextView;
 
 /**
@@ -33,7 +32,7 @@ public class CreateLoanActivity extends AppCompatActivity
         buttonBluetooth = (Button) findViewById(R.id.buttonBluetooth);
         buttonNfc = (Button) findViewById(R.id.buttonNfc);
         buttonSms = (Button) findViewById(R.id.buttonSms);
-        buttonWhatsapp= (Button) findViewById(R.id.buttonWhatsapp);
+        buttonWhatsapp= (Button) findViewById(R.id.buttonOther);
     }
 
     public void onButtonPressed (View source){
@@ -41,16 +40,16 @@ public class CreateLoanActivity extends AppCompatActivity
         switch (source.getId()){
             case R.id.buttonManualInput:
                 intent = new Intent(this,DetailActivity.class);
-                intent.putExtra("object", -1);
+                //intent.putExtra("object", -1);
                 startActivity(intent);
             break;
             case R.id.buttonBluetooth:
-                //an dieser Stelle Bluetooth aktivieren
+                /*Uri bluetooth = Uri.parse("URL");  //URL parsen
                 Intent sendIntent = new Intent();
                 sendIntent.setAction(Intent.ACTION_SEND);
-                sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+                sendIntent.putExtra(Intent.EXTRA_TEXT, bluetooth);
                 sendIntent.setType("text/plain");
-                startActivity(Intent.createChooser(sendIntent, "Titel"));
+                startActivity((sendIntent, "Titel"));*/
                 break;
             case R.id.buttonNfc:
                 //an dieser Stelle NFC aktivieren
@@ -63,10 +62,13 @@ public class CreateLoanActivity extends AppCompatActivity
                 //intent.putExtra("object", -1);
                 //startActivity(intent);
                 break;
-            case R.id.buttonWhatsapp:
-                //intent = new Intent(this,DetailActivity.class);
-                //intent.putExtra("object", -1);
-                //startActivity(intent);
+            case R.id.buttonOther:
+                Uri adress = Uri.parse("URL");  //URL parsen
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, adress);
+                sendIntent.setType("text/plain");
+                startActivity(Intent.createChooser(sendIntent, "Titel"));
                 break;
         }
     }
