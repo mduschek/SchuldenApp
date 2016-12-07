59dd6c2af16887fd376bb4aa65d306be02729b01
mduschek <duschek54321@gmail.com>
2016-12-07 12:50:18 +0100

    deep links v1

--- SchuldenApp/app/src/main/java/at/htlgkr/raiffeisenprojektteam/schuldenapp/CreateLoanActivity.java
+++ SchuldenApp/app/src/main/java/at/htlgkr/raiffeisenprojektteam/schuldenapp/CreateLoanActivity.java
@@ -10,6 +10,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Created by Alexander on 21.11.16.
  */
@@ -63,12 +66,20 @@ public class CreateLoanActivity extends AppCompatActivity
                 //startActivity(intent);
                 break;
             case R.id.buttonOther:
-                Uri adress = Uri.parse("URL");  //URL parsen
+
+
+                Uri adress = Uri.parse("schuldenapp://createloan");  //URL parsen
                 Intent sendIntent = new Intent();
                 sendIntent.setAction(Intent.ACTION_SEND);
-                sendIntent.putExtra(Intent.EXTRA_TEXT, adress);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "http://at.htlgkr.schuldenapp.createloan/schuldenapp");
+                //sendIntent.putExtra(Intent.EXTRA_ORIGINATING_URI, adress);
                 sendIntent.setType("text/plain");
-                startActivity(Intent.createChooser(sendIntent, "Titel"));
+                startActivity(Intent.createChooser(sendIntent, "App zum Senden auswählen"));
+
+                /*Intent sendIntent = new Intent();
+                sendIntent.setData (Uri.parse("schuldenapp://createloan"));
+                startActivity(Intent.createChooser(sendIntent, "Titel"));*/
+
                 break;
         }
     }
