4644d5139cf75e88ef62548d353c8ffab1da1066
Alexander Perndorfer <alexander-perndorfer@hotmail.com>
2016-12-14 12:36:15 +0100

    Style

--- SchuldenApp/app/src/main/java/at/htlgkr/raiffeisenprojektteam/schuldenapp/DetailActivity.java
+++ SchuldenApp/app/src/main/java/at/htlgkr/raiffeisenprojektteam/schuldenapp/DetailActivity.java
@@ -1,31 +1,120 @@
 package at.htlgkr.raiffeisenprojektteam.schuldenapp;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcEvent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by michael on 24.11.16.
  */
 
-public class DetailActivity extends AppCompatActivity
+public class DetailActivity extends AppCompatActivity implements NfcAdapter.CreateNdefMessageCallback
 {
-    EditText iban, firstname, lastname, value,usuage;
-    Button btnOk;
+    TextView textViewCreateLoanDescription;
+    Button buttonManualInput, buttonBluetooth, buttonNfc, buttonOther;
+    public static final String LINK = "http://at.htlgkr.schuldenapp.createloan/schuldenapp";
+    SharedPreferences sharedPreferences;
+    //STRUKTUR: ?content=Michael;Duschek;Usuage;IBAN;30.65
+    private static final String TAG = "DetailActivity";
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_detail_create);
-        Dept d = (Dept) getIntent().getSerializableExtra("object");
 
-        iban = (EditText) findViewById(R.id.editTextIban);
-        firstname = (EditText) findViewById(R.id.editTextFirstName);
-        lastname = (EditText) findViewById(R.id.editTextLastName);
-        value = (EditText) findViewById(R.id.editTextSum);
-        usuage = (EditText) findViewById(R.id.editTextUsuage);
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        textViewCreateLoanDescription = (TextView) findViewById(R.id.textViewCreateLoanDescription);
 
-        btnOk = (Button) findViewById(R.id.btnOk);
+        buttonBluetooth = (Button) findViewById(R.id.buttonBluetooth);
+        buttonNfc = (Button) findViewById(R.id.buttonNfc);
+        buttonOther= (Button) findViewById(R.id.buttonOther);
+
+        //NFC
+        if (!MainActivity.nfcIsAvailable) buttonNfc.setVisibility(View.GONE);
+        MainActivity.nfcAdapter.setNdefPushMessageCallback(this,this);
+    }
+
+    public void onButtonPressed (View source){
+        Intent intent;
+        switch (source.getId()){
+            case R.id.buttonManualInput:
+
+                intent = new Intent(this,DetailActivity.class);
+
+                //intent.putExtra("object", -1);
+                startActivity(intent);
+                break;
+            case R.id.buttonBluetooth:
+
+                intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, "test msg");
+                intent.setType("text/plain");
+
+                //startActivity(Intent.createChooser(intent, "Titel"));
+
+                /*Uri bluetooth = Uri.parse("URL");  //URL parsen
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, bluetooth);
+                sendIntent.setType("text/plain");
+                startActivity((sendIntent, "Titel"));*/
+
+                break;
+            case R.id.buttonNfc:
+                //an dieser Stelle NFC aktivieren
+                //intent = new Intent(this,DetailActivity.class);
+                //intent.putExtra("object", -1);
+                //startActivity(intent);
+
+                break;
+            case R.id.buttonSms:
+                //intent = new Intent(this,DetailActivity.class);
+                //intent.putExtra("object", -1);
+                //startActivity(intent);
+                break;
+            case R.id.buttonOther:
+
+
+                Uri adress = Uri.parse("schuldenapp://createloan");  //URL parsen
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, LINK+"?content=Alexander;Perndorfer;Essen;AT34442566756567;30.65");
+                //sendIntent.putExtra(Intent.EXTRA_ORIGINATING_URI, adress); //geändert
+                sendIntent.setType("text/plain");
+                startActivity(Intent.createChooser(sendIntent, "App zum Senden auswählen"));
+
+                /*Intent sendIntent = new Intent();
+                sendIntent.setData (Uri.parse("schuldenapp://createloan"));
+                startActivity(Intent.createChooser(sendIntent, "Titel"));*/
+
+                break;
+        }
+    }
+
+    @Override
+    public NdefMessage createNdefMessage(NfcEvent nfcEvent) {
+        final String stringOut = "Michael;Duschek;Usuage;IBAN;30.65";
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(),stringOut,Toast.LENGTH_LONG).show();
+            }
+        });
+        NdefRecord ndefRecord = NdefRecord.createMime("text/plain", stringOut.getBytes());
+        NdefMessage ndefMessage = new NdefMessage(ndefRecord);
+        return ndefMessage;
     }
 }
