d2de40972f07124bfc96a0adc3c0954379541269
Pawel <p.tutakiewicz@gmail.com>
2017-01-11 12:38:28 +0100

    QR in SchuldenApp imple.

---
+++ SchuldenApp/app/src/main/java/at/htlgkr/raiffeisenprojektteam/schuldenapp/QrScannerActivity.java
@@ -0,0 +1,42 @@
+package at.htlgkr.raiffeisenprojektteam.schuldenapp;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+public class QrScannerActivity extends AppCompatActivity {
+
+    ImageView imgVw;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_scanner);
+
+        imgVw= (ImageView) findViewById(R.id.imgView);
+
+        Bundle extras = getIntent().getExtras();
+        String data="";
+
+        if (extras != null) {
+            data = extras.getString("qr");
+        }
+
+        MultiFormatWriter multiFormatWriter=new MultiFormatWriter();
+
+        try {
+            BitMatrix bitMatrix = multiFormatWriter.encode(data, BarcodeFormat.QR_CODE,200,200); //boom schaka laka
+            BarcodeEncoder barcodeEncoder=new BarcodeEncoder(); //simsalabim
+            Bitmap bitmap=barcodeEncoder.createBitmap(bitMatrix);//expiliamos
+            imgVw.setImageBitmap(bitmap);//stupor
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+    }
+}
