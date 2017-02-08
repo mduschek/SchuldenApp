e769c4dd8dd3e204733e6aaa1c29406691455e74
mduschek <duschek54321@gmail.com>
2017-02-08 11:55:40 +0100

    ArchiveActivity erstellen

---
+++ SchuldenApp/app/src/main/java/at/htlgkr/raiffeisenprojektteam/schuldenapp/ArchiveActivity.java
@@ -0,0 +1,16 @@
+package at.htlgkr.raiffeisenprojektteam.schuldenapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by michael on 08.02.17.
+ */
+
+public class ArchiveActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_scanner);
+    }
+}
