3d10209460fe0d8e746aa426c108b3465db60222
Alexander Perndorfer <alexander-perndorfer@hotmail.com>
2017-04-24 12:15:26 +0200

    Tests

--- SchuldenApp/app/src/androidTest/java/at/htlgkr/raiffeisenprojektteam/schuldenapp/ExampleInstrumentedTest.java
+++ SchuldenApp/app/src/androidTest/java/at/htlgkr/raiffeisenprojektteam/schuldenapp/ExampleInstrumentedTest.java
@@ -27,46 +27,60 @@ public class ExampleInstrumentedTest {
 
     SQLiteDatabase db;
     Context appContext = InstrumentationRegistry.getTargetContext();
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-        db  = new DebtsDbHelper(appContext).getWritableDatabase();
+        db = new DebtsDbHelper(appContext).getWritableDatabase();
         assertEquals("at.htlgkr.raiffeisenprojektteam.schuldenapp", appContext.getPackageName());
         ContentValues cv = new ContentValues();
-        cv.put(TblDebts.I_AM_CREDITOR,true);
-        cv.put(TblDebts.VALUE,666);
-        cv.put(TblDebts.LASTNAME,"Test");
-        cv.put(TblDebts.FIRSTNAME,"Unit");
+        cv.put(TblDebts.I_AM_CREDITOR, true);
+        cv.put(TblDebts.VALUE, 666);
+        cv.put(TblDebts.LASTNAME, "Test");
+        cv.put(TblDebts.FIRSTNAME, "Unit");
         cv.put(TblDebts.DATE, new Date().toString());
-        cv.put(TblDebts.IBAN,"UnitTestIban");
-        cv.put(TblDebts.USAGE,"Testverwendung");
+        cv.put(TblDebts.IBAN, "UnitTestIban");
+        cv.put(TblDebts.USAGE, "Testverwendung");
         cv.put(TblDebts.STATUS, "open");
-        appContext.getContentResolver().insert(DebtsContentProvider.DEBT_URI,cv);
-        Cursor c =  db.rawQuery("SELECT * FROM "+TblDebts.TABLE_NAME+" ORDER BY "+TblDebts.ID+";",null);
+        appContext.getContentResolver().insert(DebtsContentProvider.DEBT_URI, cv);
+        Cursor c = db.rawQuery("SELECT * FROM " + TblDebts.TABLE_NAME + " ORDER BY " + TblDebts.ID + ";", null);
         c.moveToLast();
 
-        Log.w("*=TESTEN=*",c.getInt(c.getColumnIndex(TblDebts.VALUE))+"");
-        assertEquals(666,c.getInt(c.getColumnIndex(TblDebts.VALUE)));
+        Log.w("*=TESTEN=*", c.getInt(c.getColumnIndex(TblDebts.VALUE)) + "");
+        assertEquals(666, c.getInt(c.getColumnIndex(TblDebts.VALUE)));
 
     }
 
     @Test
-    public void updateTest() throws Exception{
+    public void updateTest() throws Exception {
         Cursor c;
         ContentValues cv;
 
-        db  = new DebtsDbHelper(appContext).getWritableDatabase();
-        c =  db.rawQuery("SELECT * FROM "+TblDebts.TABLE_NAME+" ORDER BY "+TblDebts.ID+";",null);
+        db = new DebtsDbHelper(appContext).getWritableDatabase();
+        c = db.rawQuery("SELECT * FROM " + TblDebts.TABLE_NAME + " ORDER BY " + TblDebts.ID + ";", null);
         c.moveToLast();
         int id = c.getInt(c.getColumnIndex(TblDebts.ID));
         c.close();
-        cv=new ContentValues();
-        cv.put(TblDebts.STATUS,"not_paid");
-        appContext.getContentResolver().update(DebtsContentProvider.DEBT_URI,cv,TblDebts.ID+" = "+id,null);
-        c =  db.rawQuery("SELECT * FROM "+TblDebts.TABLE_NAME+" ORDER BY "+TblDebts.ID+";",null);
+        cv = new ContentValues();
+        cv.put(TblDebts.STATUS, "not_paid");
+        appContext.getContentResolver().update(DebtsContentProvider.DEBT_URI, cv, TblDebts.ID + " = " + id, null);
+        c = db.rawQuery("SELECT * FROM " + TblDebts.TABLE_NAME + " ORDER BY " + TblDebts.ID + ";", null);
         c.moveToLast();
         String status = c.getString(c.getColumnIndex(TblDebts.STATUS));
-        assertEquals("not_paid",status);
+        assertEquals("not_paid", status);
     }
+
+    @Test
+    public void queryTest () throws Exception
+    {
+        Cursor c = appContext.getContentResolver().query(DebtsContentProvider.DEBT_URI,null,null,null,null);
+
+        boolean b = false;
+        if(c.getCount()>0) b=true;
+
+        assertTrue(b);
+    }
+
+
 }
