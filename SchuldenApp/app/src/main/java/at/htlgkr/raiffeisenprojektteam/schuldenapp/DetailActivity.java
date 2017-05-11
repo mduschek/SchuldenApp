95b418b3b1ec255b8c6e8cc6fb58fa8bb604c69f
mduschek <duschek54321@gmail.com>
2017-05-11 09:14:32 +0200

    enspurt #1

--- SchuldenApp/app/src/main/java/at/htlgkr/raiffeisenprojektteam/schuldenapp/DetailActivity.java
+++ SchuldenApp/app/src/main/java/at/htlgkr/raiffeisenprojektteam/schuldenapp/DetailActivity.java
@@ -368,6 +368,7 @@ public class DetailActivity extends AppCompatActivity {
             buttonConfirmPayment.setVisibility(View.GONE);
             buttonGenerateQrCode.setVisibility(View.GONE);
             buttonOther.setVisibility(View.GONE);
+            textViewCreateLoanDescription.setVisibility(View.GONE);
         }
     }
 
